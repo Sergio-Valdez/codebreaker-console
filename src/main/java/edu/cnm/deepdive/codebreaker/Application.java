0209af2f2c8d346d2305cbc8c90d1e4885216b2e
Sergio Valdez <sergiovaldez159@yahoo.com>
2021-10-07 10:13:17 -0600

    Static non-final

--- src/main/java/edu/cnm/deepdive/codebreaker/Application.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/Application.java
@@ -2,45 +2,38 @@ package edu.cnm.deepdive.codebreaker;
 
 import edu.cnm.deepdive.codebreaker.model.Game;
 import edu.cnm.deepdive.codebreaker.service.GameRepository;
-import edu.cnm.deepdive.codebreaker.service.WebServiceProxy;
 import java.io.IOException;
-import retrofit2.Call;
-import retrofit2.Response;
 
 public class Application {
 
-  private static final String DEFUALT_POOL = "ABCDEF";
-  private static final int DEFUALT_LENGTH = 3;
+  private static final String DEFAULT_POOL = "ABCDEF";
+  private static final int DEFAULT_LENGTH = 3;
 
+  // Static non-final
   private final GameRepository repository;
-  private final String pool;
-  private final int length;
-  private final Game game;
+  private Game game;
 
-  private Application(String[] args) {
-    if (args.length > 0) {
-      pool = args[0];
-      if (args.length > 1) {
+  private Application(String[] args) throws IOException {
+    String pool = DEFAULT_POOL;
+    int length = DEFAULT_LENGTH;
+    switch (args.length) {
+      default:
+        // Deliberate fall-through
+      case 2:
         length = Integer.parseInt(args[1]);
-      } else {
-        length = DEFUALT_LENGTH;
-      }
-    } else {
-      pool = DEFUALT_POOL;
-      length = DEFUALT_LENGTH;
-      }
-
+        // Deliberate fall-through!
+      case 1:
+        pool = args[0];
+        break;
+      case 0:
+        // Do nothing
+    }
     repository = new GameRepository();
     game = repository.startGame(pool, length);
 
   }
-
   public static void main(String[] args) throws IOException {
-    // TODO read command-line arguments for pool & length.
-    String pool = "ABCDEF"; // FIXME
-    int length=3; //FIXME Read from args
-    Game game = startGame(pool, length);
-    System.out.printf("Game id = %s%n", game.getId());
+    Application application = new Application(args);
     // TODO while code is not guessed:
     // 1. Read guess from user input
     // 2. Submit guess to codebreaker service
